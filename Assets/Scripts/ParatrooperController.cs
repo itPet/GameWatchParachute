bc960fbf67413846112faec3cd0bf2579df11416
Peter Karlsson <peter.karlsson90@icloud.com>
2018-08-24 16:06:33 +0200

    Added moving paratrooper

---
+++ Assets/Scripts/ParatrooperController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParatrooperController : MonoBehaviour {
+
+    public Transform trooperPositions;
+    int currentPosition = 0;
+    float moveDelay = 0.5f;
+
+    private void Start() {
+        transform.position = trooperPositions.GetChild(currentPosition).position;
+        StartCoroutine(MoveTrooper());
+    }
+
+    IEnumerator MoveTrooper() {
+        while (true) {
+            yield return new WaitForSeconds(moveDelay);
+            if (currentPosition < trooperPositions.childCount - 1)
+                currentPosition++;
+            else
+                currentPosition = 0;
+            transform.position = trooperPositions.GetChild(currentPosition).position;
+        }
+    }
+
+}
