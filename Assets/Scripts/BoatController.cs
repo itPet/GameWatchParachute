3ec467b0fafac7c952fefce7600e7f1259c426c4
Peter Karlsson <peter.karlsson90@icloud.com>
2018-08-24 15:22:47 +0200

    Can move boat

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour {
+
+    public Transform boatPositions;
+    SpriteRenderer spriteRenderer;
+    int currentPosition = 0;
+
+    private void Start() {
+        transform.position = boatPositions.GetChild(currentPosition).position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable() {
+        InputsController.ClickedLeft += MoveLeft;
+        InputsController.ClickedRight += MoveRight;
+    }
+
+    private void OnDisable() {
+        InputsController.ClickedLeft -= MoveLeft;
+        InputsController.ClickedRight -= MoveRight;
+    }
+
+    void MoveLeft() {
+        if (currentPosition > 0)
+            currentPosition--;
+        
+        transform.position = boatPositions.GetChild(currentPosition).position;
+        spriteRenderer.flipX = false;
+    }
+
+    void MoveRight() {
+        if (currentPosition < boatPositions.childCount - 1)
+            currentPosition++;
+        
+        transform.position = boatPositions.GetChild(currentPosition).position;
+        spriteRenderer.flipX = true;
+    }
+}
---
+++ Assets/Scripts/InputsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputsController : MonoBehaviour {
+
+    public bool isLeft;
+    public delegate void PlayerClicked();
+    public static event PlayerClicked ClickedLeft;
+    public static event PlayerClicked ClickedRight;
+
+    private void OnMouseDown()
+    {
+        if (isLeft)
+            ClickedLeft();
+        else
+            ClickedRight();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
